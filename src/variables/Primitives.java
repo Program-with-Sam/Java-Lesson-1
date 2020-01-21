4109fc9aa75b48c9b13833cc3d95e13f657662e9
Sam <sam@malpass.me.uk>
2020-01-21 14:17:58 +0000

    Add primitive variables

---
+++ src/variables/Primitives.java
@@ -0,0 +1,29 @@
+package variables;
+
+public class Primitives {
+
+    // This is 8-bits in size which can represent an integer in the range of -128 to 127
+    byte exampleByte = 60;
+
+    // This is 16-bits in size which can take a minimum value of 0 and a maximum of 65,535 and is used to represent any Unicode character
+    char exampleChar = 'a';
+
+    // This is 16-bits in size that represents integers in the range -32,768 to 32,768
+    short exampleShort = 32000;
+
+    // This is 32-bits in size that represents integers in the range -2,147,483,648 to 2,147,483,647
+    int exampleInt = 10;
+
+    // This is 64-bits in size that represents integers in the range -9,223,372,036,854,775,808 to +9,223,372,036,854,775,807
+    long exampleLong = 65L;
+
+    // This is 32-bits in size and represents a single precision floating point (decimal) number. This means it can represent decimal numbers in the range 3.402,823,5 E+38 to 1.4 E-45
+    float exampleFloat = 26.5f;
+
+    // This is 64-bits in size and represents a double precision floating point (decimal) number. This means it can represent decimal numbers in the range 1.797,693,134,862,315,7 E+308 to 4.9 E-324
+    double exampleDouble = 65.5;
+
+    // This is a simple flag variable that can take the value true or false
+    boolean exampleBoolean = true;
+
+}
